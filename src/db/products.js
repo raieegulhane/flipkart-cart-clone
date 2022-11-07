fb5d6768e9ad876e5cc162c509a9febc8e9b8081
raieegulhane <gulhaneraiee17@gmail.com>
2022-11-07 19:44:51 +0530

    db

---
+++ src/db/products.js
@@ -0,0 +1,134 @@
+export const products = [
+    {
+        id: 1,
+        productName: "Full Sleeve Solid Men Casual Jacket",
+        brand: "BEING HUMAN",
+        oriPrice: 5999,
+        discPrice: 2400,
+        discount: 59,
+        size: ["S", "M", "XL", "XXL"],
+        fcAssured: true,
+        productImg: "https://rukminim1.flixcart.com/image/832/832/xif0q/jacket/h/o/o/l-yes-bhji21514-olive-being-human-original-imaggw4q9pequ5yy.jpeg?q=70"
+    },
+    {
+        id: 2,
+        productName: "Full Sleeve Solid Men Casual Jacket",
+        brand: "DUCATI",
+        oriPrice: 1649,
+        discPrice: 5499,
+        discount: 70,
+        size: ["XS", "S", "M", "L", "XL", "XXL"],
+        fcAssured: true,
+        productImg: "https://rukminim1.flixcart.com/image/832/832/kgv5x8w0/jacket/h/u/t/m-dtaw20jk003a-ducati-original-imafxyfzfxfcv6ry.jpeg?q=70" 
+    },
+    {
+        id: 3,
+        productName: "Full Sleeve Checkered Men Bomber Jacket",
+        brand: "Roadster",
+        oriPrice: 1336,
+        discPrice: 3299,
+        discount: 59,
+        size: ["XS", "S", "M", "L", "XL", "XXL"],
+        fcAssured: false,
+        productImg: "https://rukminim1.flixcart.com/image/832/832/xif0q/jacket/q/f/m/xxl-9564665-roadster-original-imafzp8gemskxcwg-bb.jpeg?q=70" 
+    },
+    {
+        id: 4,
+        productName: "Full Sleeve Solid Men Varsity Jacket",
+        brand: "Mast & Harbour ",
+        oriPrice: 1575,
+        discPrice: 3999,
+        discount: 60,
+        size: ["XS", "S", "M", "L", "XL", "XXL"],
+        fcAssured: true,
+        productImg: "https://rukminim1.flixcart.com/image/832/832/kwpam4w0/jacket/3/d/t/s-no-14364352-mast-harbour-original-imag9bnfh7ay3zqx.jpeg?q=70" 
+    },
+    {
+        id: 5,
+        productName: "Sleeveless Colorblock Men Quilted Jacket",
+        brand: "U.S. POLO ASSN.",
+        oriPrice: 2921,
+        discPrice: 4499,
+        discount: 35,
+        size: ["XS", "S", "M", "L", "XL", "XXL"],
+        fcAssured: false,
+        productImg: "https://rukminim1.flixcart.com/image/832/832/xif0q/jacket/q/p/n/m-1-no-usjcks0235-u-s-polo-association-original-imagg49yyaqjnznj.jpeg?q=70" 
+    },
+    {
+        id: 6,
+        productName: "PUMA",
+        brand: "Full Sleeve Printed Men Casual Jacket",
+        oriPrice: 1599,
+        discPrice: 3999,
+        discount: 60,
+        size: ["XS", "S", "M", "L", "XL", "XXL"],
+        fcAssured: true,
+        productImg: "https://rukminim1.flixcart.com/image/832/832/ksoz53k0/jacket/d/q/u/m-1-no-67029102-puma-original-imag67esprxubgzz.jpeg?q=70" 
+    },
+    {
+        id: 7,
+        productName: "ESS Track TR Full Sleeve Solid Men Casual Jacket",
+        brand: "PUMA",
+        oriPrice: 1199,
+        discPrice: 2999,
+        discount: 60,
+        size: ["XS", "S", "M", "L", "XL", "XXL"],
+        fcAssured: false,
+        productImg: "https://rukminim1.flixcart.com/image/832/832/xif0q/jacket/h/x/t/xs-58669603-puma-original-imagyw6y5fdqgdjq-bb.jpeg?q=70" 
+    },
+    {
+        id: 8,
+        productName: "M Flc Jkt 3 Full Sleeve Colorblock Men Casual Jacket",
+        brand: "Wildcraft",
+        oriPrice: 1280,
+        discPrice: 2099,
+        discount: 39,
+        size: ["XS", "S", "M", "L", "XL", "XXL"],
+        fcAssured: false,
+        productImg: "https://rukminim1.flixcart.com/image/832/832/xif0q/jacket/t/g/4/-original-imagj78ewhffzz6b.jpeg?q=70" 
+    },
+    {
+        id: 9,
+        productName: "Full Sleeve Solid Men Puffer Jacket",
+        brand: "Sweven",
+        oriPrice: 999,
+        discPrice: 2799,
+        discount: 64,
+        size: ["XS", "S", "M", "L", "XL", "XXL"],
+        fcAssured: true,
+        productImg: "https://rukminim1.flixcart.com/image/832/832/l52sivk0/jacket/r/v/w/m-1-no-sw-9101-tan-sweven-original-imagftvghaa5xbgx.jpeg?q=70" 
+    },
+    {
+        id: 10,
+        productName: "Full Sleeve Solid Men Casual Jacket",
+        brand: "MONTE CARLO",
+        oriPrice: 3132,
+        discPrice: 4475,
+        discount: 30,
+        size: ["XS", "S", "M", "L", "XL", "XXL"],
+        fcAssured: true,
+        productImg: "https://rukminim1.flixcart.com/image/832/832/ki6bgcw0-0/jacket/v/b/l/m-220050224-5-monte-carlo-original-imafyyr7y49knhhg.jpeg?q=70" 
+    },
+    {
+        id: 11,
+        productName: "M Flc Jkt 2 Full Sleeve Colorblock Men Casual Jacket",
+        brand: "Wildcraft",
+        oriPrice: 1427,
+        discPrice: 2099,
+        discount: 32,
+        size: ["XS", "S", "M", "L", "XL", "XXL"],
+        fcAssured: true,
+        productImg: "https://www.flipkart.com/wildcraft-full-sleeve-colorblock-men-jacket/p/itm2f1553f1ad7cb?pid=JCKFJFYNZVYBW6C2&lid=LSTJCKFJFYNZVYBW6C2SJQQ0D&marketplace=FLIPKART&store=clo%2Fqvw%2Fz0g%2Fjbm&srno=b_3_93&otracker=browse&fm=organic&iid=1805d6bf-7ad4-4f82-922d-7d5889258f9a.JCKFJFYNZVYBW6C2.SEARCH&ppt=browse&ppn=browse&ssid=m2v28fousrw2sveo1667826532198" 
+    },
+    {
+        id: 12,
+        productName: "RUN ICONS JKT M Full Sleeve Self Design Men Sports Jacket",
+        brand: "ADIDAS",
+        oriPrice: 5251,
+        discPrice: 6299,
+        discount: 16,
+        size: ["XS", "S", "M", "L", "XL", "XXL"],
+        fcAssured: false,
+        productImg: "https://rukminim1.flixcart.com/image/832/832/xif0q/jacket/f/b/s/m-1-no-hn2877-adidas-original-imaghgzug9hkttrq.jpeg?q=70" 
+    },
+]   
